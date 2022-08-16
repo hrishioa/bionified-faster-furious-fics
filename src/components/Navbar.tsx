b9eaa6bce9393b8d5aed52d59bb825ad73177c4c
Hrishi Olickel <hrishioa@gmail.com>
2022-08-16 18:17:53 +0800

    Working quickbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,8 @@ import { CommandBarIcon } from './CommandBar/CommandBarIcon';
 
 export const NavBar: React.FC<any> = () => {
   const [theme, setTheme] = useState('light');
+  const [bottomBarEnabled, setBottomBarEnabled] = useState(false);
+  const [bottomBarShown, setBottomBarShown] = useState(false);
 
   useEffect(() => {
     if (document) {
@@ -11,21 +13,52 @@ export const NavBar: React.FC<any> = () => {
     }
   }, [theme]);
 
-  return (
-    <div className="navbar">
-      <DarkModeSwitch
-        checked={theme === 'dark'}
-        style={{
-          margin: '10px',
-        }}
-        onChange={() =>
-          setTheme((theme) => (theme === 'light' ? 'dark' : 'light'))
+  useEffect(() => {
+    let pastScroll = window.scrollY;
+
+    const scrollHandler = () => {
+      if(window.scrollY > 100) {
+        setBottomBarEnabled(true);
+      } else {
+        setBottomBarEnabled(false);
+      }
+
+      if(Math.abs(pastScroll - window.scrollY) > 50) {
+        if(pastScroll > window.scrollY) {
+          setBottomBarShown(true);
+        } else {
+          setBottomBarShown(false);
         }
-        size={50}
-        moonColor="var(--heading-color)"
-        sunColor="var(--heading-color)"
-      />
+
+        pastScroll = window.scrollY;
+      }
+    }
+
+    window.addEventListener('scroll', scrollHandler, {passive: true});
+
+    return () => window.removeEventListener('scroll', scrollHandler);
+  });
+
+  return (
+    <>
+        {bottomBarEnabled && <div style={{
+          height: '80px'
+        }}></div>}
+        <div className={`navbar ${bottomBarEnabled ? 'bottom': ''} ${bottomBarShown && bottomBarEnabled ? 'bottom-shown': ''}`}>
+        <DarkModeSwitch
+          checked={theme === 'dark'}
+          style={{
+            margin: '10px',
+          }}
+          onChange={() =>
+            setTheme((theme) => (theme === 'light' ? 'dark' : 'light'))
+          }
+          size={50}
+          moonColor="var(--heading-color)"
+          sunColor="var(--heading-color)"
+        />
       <CommandBarIcon />
-    </div>
+      </div>
+    </>
   );
 };
