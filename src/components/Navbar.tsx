8a7af782ac4f64489a89f1b834d51045cc33ed0c
Hrishi Olickel <hrishioa@gmail.com>
2022-08-20 04:35:42 +0800

    Added bookmarklet

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -69,6 +69,16 @@ export const NavBar: React.FC<any> = () => {
           bottomBarShown && bottomBarEnabled ? 'bottom-shown' : ''
         }`}
       >
+        <div className='bookmarklet' dangerouslySetInnerHTML={{
+          __html: `
+            <a class="hidden-bookmarklink" href="javascript:(window.location.toString().match('archiveofourown.org/works')?window.location=(window.location.toString().replace('archiveofourown.org','archiveofherown.org')):alert('Use me on a real AO3 Work to jump to BF3!'))">
+            AO3 -> BF3 Portal
+            </a>
+            <span>Bookmark Me!</span>
+          `
+        }}>
+        </div>
+        <div className='flexseparator'></div>
         <div className="username" onClick={openLogoutOption}>
           {username || 'Anonymous'}
         </div>
